9c78ce54427c1f29551f9b4fbe8f42790dd6585a
Jeferson_Vasconez <Jhvasconez@espe.edu.ec>
2026-01-12 22:12:07 -0500

    V 1.0

---
+++ Documents/Proyectos net/ModDeportista/src/moddeportista/RendimientoService.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package moddeportista;
+
+/**
+ *
+ * @author labesp
+ */
+public interface RendimientoService {
+    double calcularPromedio(double[] marcas);
+    boolean esRendimientoAlto(double promedio);
+
+}
